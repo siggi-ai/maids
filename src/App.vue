513359a0635b6b3f0434d039a311fab14309c28b
Siegfried Hamm <siegfried.hamm@gmail.com>
2024-02-11 21:46:06 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex items-center justify-center">
+    <img :src=apiResponseData.images[0].url alt="waifu" class="w-96">
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      apiResponseData: {},
+    };
+  },
+
+  created: async function () {
+    const apiUrl = `https://api.waifu.im/search`;
+    const params = {
+      included_tags: "maid",
+      height: ">=2000",
+    };
+    const queryParams = new URLSearchParams(params);
+    const requestUrl = `${apiUrl}?${queryParams}`;
+
+    try {
+      const response = await fetch(requestUrl);
+      const result = await response.json();
+      this.apiResponseData = result;
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
