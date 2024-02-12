6a8c695f333819f4d26844eabec7eda340f3d1dc
Siegfried Hamm <siegfried.hamm@gmail.com>
2024-02-12 07:39:39 +0100

    method to fetch by click

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="flex items-center justify-center">
-    <img :src=apiResponseData.images[0].url alt="waifu" class="w-96">
+    <button
+      class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-300 rounded"
+      v-on:click="getWaifu"
+    >
+      <img :src="apiResponseData.images[0].url" alt="waifu" class="w-96"/>click to update
+    </button>
   </div>
 </template>
 
 <script>
 export default {
+  emits: ["onClick"],
   data: function () {
     return {
       apiResponseData: {},
@@ -30,6 +36,30 @@ export default {
       console.log(error);
     }
   },
+  methods: {
+    async getWaifu () {
+      console.log("hey!");
+      const apiUrl = `https://api.waifu.im/search`;
+      const params = {
+      included_tags: "maid",
+      height: ">=2000",
+    };
+      console.log(apiUrl);
+
+      const queryParams = new URLSearchParams(params);
+      const requestUrl = `${apiUrl}?${queryParams}`;
+
+      try {
+      const response = await fetch(requestUrl);
+      const result = await response.json();
+      this.apiResponseData = result;
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+    }
+  },
+  
 };
 </script>
 
