e65b5861c547f09eda89627a29c5115dd8c9b948
Siegfried Hamm <siegfried.hamm@gmail.com>
2024-02-14 09:01:56 +0100

    random not just pretense
    while before the tags were hard coded
    now there is an actual random-function there
    at least in the click-event
    and the text in button is not bold anymore

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,28 @@
 <template>
   <div class="flex items-center justify-center bg-rose-300 h-screen">
+
     <!-- this is the image, which is also a button to click on -->
     <button
-      class="bg-pink-600 hover:bg-pink-700 text-white font-bold py-2 px-4 border border-red-300 rounded-md font-bangers text-2xl"
-      v-on:click="getWaifu"
+      class="bg-pink-600 hover:bg-pink-700 text-white py-2 px-4 border border-red-300 rounded-md font-bangers text-2xl"
+      v-on:click="getWaifu($event); random_waifu_tag();"
     >
       click image to update (random)
-      <img :src="apiResponseData.images[0].url" alt="waifu" class="h-[80vh]" />
+      <img :src="apiResponseData.images[0].url" alt="waifu" class="h-[80vh]"/>
     </button>
+
 
     <!-- load image by selected option -->
 
-    <div class="flex flex-col p-8">
-      <label for="tag" class="text-center font-bangers"
-        >Choose an Option:</label
-      >
-      <select
-        name="waifu"
-        v-on:change="selectWaifu($event)"
-        v-model="tag"
-        class="w-36 py-3 px-4 pe-9 block border-gray-200 rounded-lg text-sm focus:border-red-500 focus:ring-red-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-pink-700 dark:text-pink-400 dark:focus:ring-pink-300"
-      >
+    <div class="flex flex-col p-8 pt-8">
+      <label for="tag" class="text-center font-bangers">Choose an Option:</label>
+      <select name="waifu" v-on:change="selectWaifu($event)" v-model="tag" class="w-36 py-3 px-4 pe-9 block border-gray-200 rounded-lg text-sm focus:border-red-500 focus:ring-red-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-pink-700 dark:text-pink-400 dark:focus:ring-pink-300">
         <option :value="tag" v-for="tag in tags">{{ tag }}</option>
       </select>
     </div>
+
+
+
+
   </div>
 </template>
 
@@ -43,15 +42,17 @@ export default {
         "selfies",
         "uniform",
       ],
+      random_tag: [],
     };
   },
+  
 
   /* a function to do the first fetch */
 
-  created: async function () {
+    created: async function () {
     const apiUrl = `https://api.waifu.im/search`;
     const params = {
-      included_tags: "uniform",
+      included_tags: 'maid',
       height: ">=2000",
     };
     const queryParams = new URLSearchParams(params);
@@ -67,14 +68,24 @@ export default {
     }
   },
 
+
+
+
   /* function to refresh the fetch and get a random new image */
 
   methods: {
+
+    random_waifu_tag() {
+    var chosen_tag = this.tags[(Math.floor(Math.random() * this.tags.length))];
+    this.random_tag = chosen_tag;
+    },
+
     async getWaifu() {
       console.log("hey!");
+      console.log(this.random_tag)
       const apiUrl = `https://api.waifu.im/search`;
       const params = {
-        included_tags: "uniform",
+        included_tags: this.random_tag,
         height: ">=2000",
       };
       console.log(apiUrl);
@@ -91,6 +102,9 @@ export default {
         console.log(error);
       }
     },
+
+    
+    /* function to get a new image from selected category new image */
 
     async selectWaifu(event) {
       console.log("ho!");
@@ -110,7 +124,8 @@ export default {
       } catch (error) {
         console.log(error);
       }
-    },
+    }, 
+
   },
 };
 </script>
